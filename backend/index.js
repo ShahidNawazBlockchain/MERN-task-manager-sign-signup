942fff4d61029a4c9573703e7600e2c1cf4d3a6d
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-12 06:17:24 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const db = require("./db");
 const cors = require("cors");
-db();
-const app = express();
 const auth = require("./routes/auth");
 const list = require("./routes/list");
+db();
+const app = express();
 app.use(express.json());
-app.use("/api/v1", auth);
-app.use("/api/v2", list);
 app.use(cors());
 
+app.use("/api/v1", auth);
+app.use("/api/v2", list);
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`server is lisinting at ${PORT}`);
